5ba0eaefdc49f06ba5b724af0dacc31c37131502
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2025-02-06 19:24:59 +0200

    Updates

--- index.687e8a47.js
+++
@@ -1,2 +0,0 @@
-function t(t){let e=document.createElement("div");e.innerHTML=t,e.style.display="none",document.body.insertBefore(e,document.body.firstChild)}async function e(t){let e=await fetch(t);return await e.text()}!async function(){let n=['../icons/sprite.svg"'].map(t=>e(t));(await Promise.all(n)).forEach(t)}();
-//# sourceMappingURL=index.687e8a47.js.map
